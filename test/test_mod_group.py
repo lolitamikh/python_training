e375ae2d3028f1ca8d43cc7f261e2892a2076f61
lolitamikh <lolabudko@gmail.com>
2023-08-20 12:49:19 +0300

    оптимизация ввода данных в формы группы

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -2,5 +2,15 @@ from model.group import Group
 
 def test_mod_first_group(app):
     app.session.login("admin", "secret")
-    app.group.mod_first_group(Group("one","two","three"))
+    app.group.modify_first_group(Group("one", "two", "three"))
+    app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login("admin", "secret")
+    app.group.modify_first_group(Group(name="new group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login("admin", "secret")
+    app.group.modify_first_group(Group(header="new header"))
     app.session.logout()
