e695be333ccff534c8b953b4dd71467818ebb603
lolitamikh <lolabudko@gmail.com>
2023-09-01 16:04:16 +0300

    добавлены тесты для проверки телефонов

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -91,14 +92,57 @@ class ContactHelper:
 
     contact_cache = None
 
+    @property
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
             self.go_to_home_page()
             self.contact_cache = []
-            for element in wd.find_elements_by_name("entry"):
-                firstname = element.find_element_by_xpath(".//td[3]").text
-                lastname = element.find_element_by_xpath(".//td[2]").text
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+            for row in wd.find_elements_by_name("entry"):
+                cells = row.find_elements_by_tag_name("td")
+                firstname = cells[1].text
+                lastname = cells[2].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(
+                    Contact(firstname=firstname, lastname=lastname, id=id, home=all_phones[0], mobile=all_phones[1],
+                            work=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.go_to_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.go_to_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id, home=home, work=work, mobile=mobile,
+                       phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)",text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home=home, work=work, mobile=mobile,
+                       phone2=phone2)
--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -4,14 +4,14 @@ from random import randrange
 def test_mod_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = app.contact.get_contact_list
     index = randrange(len(old_contacts))
     contact = Contact("masha", "masha", "masha", "masha",
                                           "1111","111","123","12","456","678","1","","","","","")
     contact.id = old_contacts[index].id
     app.contact.mod_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = app.contact.get_contact_list
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
