b7329f4c0e5f1d5a6a8905aa44484958873757c8
lolitamikh <lolabudko@gmail.com>
2023-09-03 16:35:27 +0300

    параметризованы тесты создания групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -3,14 +3,13 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, photo = None, title=None, company=None,
-                 address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None, address2=None,
+    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
+                 address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None, homepage=None, address2=None,
                  phone2=None, notes=None, id=None, all_phones_from_home_page=None, all_emails_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
         self.nickname = nickname
-        self.photo = photo
         self.title = title
         self.company = company
         self.address = address
@@ -24,13 +23,6 @@ class Contact:
         self.email2 = email2
         self.email3 = email3
         self.homepage = homepage
-
-        self.bday = bday
-        self.bmonth = bmonth
-        self.byear = byear
-        self.aday = aday
-        self.amonth = amonth
-        self.ayear = ayear
 
         self.address2 = address2
         self.phone2 = phone2
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
+import pytest
+import random
+import string
 from model.contact import Contact
 
-    
-def test_add_new_contact(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_number(maxlen):
+    symbols = string.digits
+    return "+" + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_email(maxlen):
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@dot.com"
+
+testdata = [
+    Contact(firstname=random_string("first", 10), middlename=random_string("middle", 10), lastname=random_string("last", 10),
+            nickname=random_string("nickname", 10),  title=random_string("ttt", 10),
+            company=random_string("company", 10), address=random_string("addr1", 10),
+            home=random_number(10), mobile=random_number(10), work=random_number(10), fax=random_number(10),
+            email=random_email(10), email2=random_email(10), email3=random_email(10), homepage=random_string("www", 20),
+            address2=random_string("addr2", 10), phone2=random_number(10), notes=random_string("nnn", 20))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact("petrova", "ivanova", "anna", "anya", "title", "company", "spb", "33", "8800", "500",
-                            "123", "anya@mail.com", "www", "spb", "33", "blabla", phone2="456")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
