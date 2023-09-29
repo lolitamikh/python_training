0b98633129bd6147abf5862f81b9e19b24b27886
lolitamikh <lolabudko@gmail.com>
2023-09-29 10:16:17 +0300

    реализовано сравнение всех записей контактов с информацией из бд

--- generator/contact.py
+++ generator/contact.py
@@ -22,7 +22,7 @@ for o, a in opts:
         f = a
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "
+    symbols = string.ascii_letters + string.digits
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def random_number(maxlen):
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,4 +1,5 @@
 import re
+from fixture.contact import Contact
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
@@ -13,14 +14,18 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
 
-def test_all_info_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+def test_all_info_on_home_page(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="test"))
+    contact_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contact_from_ui = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    assert len(contact_from_db) == len(contact_from_ui)
+    for x in range(len(contact_from_ui)):
+        assert contact_from_ui[x].firstname == contact_from_db[x].firstname
+        assert contact_from_ui[x].lastname == contact_from_db[x].lastname
+        assert contact_from_ui[x].address == contact_from_db[x].address
+        assert contact_from_ui[x].all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db[x])
+        assert contact_from_ui[x].all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db[x])
 
 def clear(s):
     return re.sub("[() -]", "", s)
